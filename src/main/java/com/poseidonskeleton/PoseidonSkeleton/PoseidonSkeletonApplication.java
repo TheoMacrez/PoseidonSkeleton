93972a139dea55f265014ba6828ae7209d69ec4b
Theo Macrez <macreztheo.prog@gmail.com>
2025-01-22 15:01:26 +0100

    Initial commit

---
+++ src/main/java/com/poseidonskeleton/PoseidonSkeleton/PoseidonSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.poseidonskeleton.PoseidonSkeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseidonSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoseidonSkeletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poseidonskeleton/PoseidonSkeleton/PoseidonSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poseidonskeleton.PoseidonSkeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
